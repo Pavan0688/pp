669db1b4b649bffeda1078229b8223b461b55286
Pavan0688 <112752681+Pavan0688@users.noreply.github.com>
2022-09-07 10:13:59 +0530

    Add files via upload

---
+++ 3 class test 1.py
@@ -0,0 +1,6 @@
+a = input("enter the user name:")
+b = input("reenter the user name:")
+if (a == b):
+    print("the user name is valid:")
+else:
+    print("the user name is invalid:")
---
+++ 3 class test 2.py
@@ -0,0 +1,6 @@
+a=int(input("enter the age:"))
+if (a>=18):
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
+    print("more",18-a,"for vote")
