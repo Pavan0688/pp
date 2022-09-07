7faef2d5bd530b2fc2b359117f267a666a76afe1
Pavan0688 <112752681+Pavan0688@users.noreply.github.com>
2022-09-07 12:43:38 +0530

    Add files via upload

---
+++ 3 pp 2.py
@@ -0,0 +1,9 @@
+my_dict_1={'J':12,'W':22}
+my_dict_2={'M':67}
+print("the first dictionary is:")
+print(my_dict_1)
+print("the second dictionary is:")
+print(my_dict_2)
+my_dict_1.update(my_dict_2)
+print("the concatanated dictionary is:")
+print(my_dict_1)
